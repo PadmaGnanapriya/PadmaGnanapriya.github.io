663d006d378513a6591411116a1d5d7d1467aebe
PadmaGnanapriya <padmaisuru@gmail.com>
2021-01-23 23:44:39 +0530

    Add social media icons.

---
+++ src/componenets/AnimatedText.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AnimatedText: React.FC = () => {
+ return (
+  <React.Fragment>
+        $Start$
+  </React.Fragment>
+ );}
+
+export default AnimatedText;
--- src/componenets/Footer.tsx
+++ src/componenets/Footer.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Col, Row} from "react-bootstrap";
 
 const Footer: React.FC = () => {
   return (
-      <div className='py-3 bg-dark'>
+      <div className='py-3 bg-dark footer'>
+        <Row className="m-0">
+          <Col className="social-media py-1 py-sm-3">
+            <a href="https://www.linkedin.com/in/padma-gnanapriya-16b515180/" target="_blank" className="fa fa-linkedin"/>
+            <a href="https://www.facebook.com/padma.gnanapriya/" target="_blank" className="fa fa-facebook"/>
+            <a href="https://padmaisuru.medium.com/" target="_blank" className="fa fa-medium"/>
+            <a href="https://github.com/PadmaGnanapriya" target="_blank" className="fa fa-github"/>
+          </Col>
+
+        </Row>
         <span className='text-light'>© Designed and Developed by Padma Gnanapriya</span>
 
       </div>
