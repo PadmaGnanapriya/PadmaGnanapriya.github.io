0487f8c02c197b3a5508e9707b8b9dacbf3ba3df
PadmaGnanapriya <padmaisuru@gmail.com>
2021-01-23 23:03:28 +0530

    Change color and add gradient.

--- src/componenets/About.tsx
+++ src/componenets/About.tsx
@@ -8,11 +8,15 @@ AOS.init();
 
 const About: React.FC = () => {
   const isDark: boolean = useSelector((state: RootState) => state.isDark);
+  const viewYPosition: number = useSelector((state: RootState) => state.viewYPosition);
+
 
   return (
     <div className={isDark ? 'about-dark py-3 px-3 px-md-4 px-lg-5' : 'about py-3 px-3 px-md-4 px-lg-5'}
-         data-aos="zoom-in-up">
-      <h1 id='about' className='pt-5'>Let me tell who I am</h1>
+         data-aos="zoom-in-up"
+         style={{borderRadius: `${100-viewYPosition*20}% ${100-viewYPosition*20}% 0% 0%`}}
+    >
+      <h1 id='about' className='pt-5'>Let me tell<br/> who I am</h1>
 
       <p className='text-left'>Hello, I'm Padma Gnanapriya, Undergraduate at University of Kelaniya BSc(Hons)
         Software Engineering. I am good at being a team player as well as leading a team. By facing the dynamic
